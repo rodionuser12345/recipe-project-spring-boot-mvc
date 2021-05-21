9d8f6975f7017c8e96a391da44c56a1cd30cefb3
rodionuser12345 <79834564+rodionuser12345@users.noreply.github.com>
2021-05-21 17:51:50 +0300

    App Init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # recipe-project-spring-boot-mvc
+
 Credit to Spring Framework Guru
---
+++ src/test/java/com/rodionspringframework/recipeprojectspringbootmvc/RecipeProjectSpringBootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodionspringframework.recipeprojectspringbootmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProjectSpringBootMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
